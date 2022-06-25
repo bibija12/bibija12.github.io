7efb1010b8a66bbfbe63d15c921fe0b89f1af936
bibija12 <abiarafio@gmail.com>
2022-06-25 10:51:11 +0200

    test: alteração  do texto

--- src/pages/help.vue
+++ src/pages/help.vue
@@ -1,18 +1,25 @@
 <template>
   <q-page class="-pa-lg">
     <h5 class="q-mt-none"></h5>
-    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Recusandae officia tempora expedita perspiciatis adipisci
-ipsam et quisquam possimus, quos facilis omnis veritatis
-exercitationem. Assumenda laudantium est eos quidem dicta?</p>
-
+    <P
+      >Lorem ipsum dolor sit amet. Aut praesentium repudiandae aut rerum
+      voluptas ab quia provident At sunt laudantium et consequatur vitae? Ut
+      ipsam deleniti modi accusamus qui ullam rerum. Qui libero internos et
+      voluptatibus totam ad repellat temporibus quo praesentium amet aut dolorum
+      tenetur sed velit assumenda non natus ipsum. Sit asperiores galisum ut
+      quae porro in veritatis corporis. Eos iure harum et atque quis qui Quis
+      maiores sit ullam cupiditate sed voluptas expedita aut tempora voluptatem
+      et velit esse. Et blanditiis itaque in nihil maiores qui magnam quas qui
+      dolorum corrupti vel architecto galisum sed optio quam. Qui maxime
+      corrupti ad voluptatem Quis aut sint iste est esse delectus. Sed numquam
+      laborum et deleniti suscipit sit molestiae sunt qui ratione inventore et
+      vitae alias.
+    </P>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-export default defineComponent({
-
-});
+export default defineComponent({});
 </script>
