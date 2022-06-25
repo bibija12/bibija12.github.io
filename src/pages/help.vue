c25e595c5bd6ad4e51d64abf274166bcb67633ba
bibija12 <abiarafio@gmail.com>
2022-06-25 11:24:45 +0200

    test(UI): adicionando novo texto

--- src/pages/help.vue
+++ src/pages/help.vue
@@ -1,20 +1,9 @@
 <template>
   <q-page class="-pa-lg">
     <h5 class="q-mt-none"></h5>
-    <P
-      >Lorem ipsum dolor sit amet. Aut praesentium repudiandae aut rerum
-      voluptas ab quia provident At sunt laudantium et consequatur vitae? Ut
-      ipsam deleniti modi accusamus qui ullam rerum. Qui libero internos et
-      voluptatibus totam ad repellat temporibus quo praesentium amet aut dolorum
-      tenetur sed velit assumenda non natus ipsum. Sit asperiores galisum ut
-      quae porro in veritatis corporis. Eos iure harum et atque quis qui Quis
-      maiores sit ullam cupiditate sed voluptas expedita aut tempora voluptatem
-      et velit esse. Et blanditiis itaque in nihil maiores qui magnam quas qui
-      dolorum corrupti vel architecto galisum sed optio quam. Qui maxime
-      corrupti ad voluptatem Quis aut sint iste est esse delectus. Sed numquam
-      laborum et deleniti suscipit sit molestiae sunt qui ratione inventore et
-      vitae alias.
-    </P>
+    <P>
+      Todo vue é uma app feita para adicionar assim como eliminar lista de
+      tarefas</P>
   </q-page>
 </template>
 
